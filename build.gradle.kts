8e4993f0099ef284ae282bf20d263b087e1d7458
Leslie <LeslieDYF@gmail.com>
2023-09-25 10:17:34 +0800

    [release] 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,6 +86,7 @@ compose.desktop {
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+
             packageName = "Passwd"
             packageVersion = "1.0.0"
 
